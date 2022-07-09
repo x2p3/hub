ab4e267f2adc21ab6175a5132642d1a97f4c08ed
x2p3 <100187185+x2p3@users.noreply.github.com>
2022-07-09 20:06:39 +1000

    _scc

---
+++ ZdU2Muzy.lua
@@ -0,0 +1,30 @@
+_G.settings = {
+    ["MagicType"] = "Fire",
+    ["KillAll"] = true
+}
+ 
+local Remotes = game:GetService("ReplicatedStorage").Remotes
+local typez = _G.settings.MagicType
+local tp2 = _G.settings.TpShardsAndDia
+local tp3 = _G.settings.KillAll
+while wait() do
+local hum = game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+for i,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Main.Character.Available.ScrollingFrame.Frame:GetChildren()) do
+Remotes.DoClientMagic:FireServer(typez, v.Name)
+Remotes.DoMagic:InvokeServer(typez, v.Name)
+wait(1)
+_G.test = true
+else 
+_G.test = true
+end
+end
+end  
+if tp3 and _G.test then 
+for i,z in pairs(game.workspace:GetChildren()) do 
+if z:FindFirstChild("HumanoidRootPart") and not z:FindFirstChild("ForceField") then 
+hum.CFrame = z.HumanoidRootPart.CFrame
+end
+end
+end
+end
+end
